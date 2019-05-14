ac5450e2088f304984c3ce9e32ed179e9f36717b
OuSS <ouss.cs@gmail.com>
2019-05-14 14:13:50 +0000

    Done

--- Codable_Clean/AppDelegate.swift
+++ Codable_Clean/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let animeListController = AnimeListController(style: .plain)
+        let navController = UINavigationController(rootViewController: animeListController)
+        window?.rootViewController = navController
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Codable_Clean' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Codable_Clean
+  pod 'SDWebImage'
+  pod 'JGProgressHUD'
+
+
+end
